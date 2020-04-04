ba9b76f162e5b83e2e3e3e71e8c452da1e3a53d7
mike_van <877148452@qq.com>
2020-04-05 00:58:27 +0800

    新增五题队列相关

---
+++ src/main/java/leetcode/queue/BinaryTreeLevelOrderTraversal102.java
@@ -0,0 +1,104 @@
+package leetcode.queue;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @program: interview
+ * @author: fanmk
+ * @email: comeandgo2014@gmail.com
+ * @decription: https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * @date: 2020/4/2 23:42
+ */
+public class BinaryTreeLevelOrderTraversal102 {
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.add(root);
+        queue.add(null);
+        res.add(new ArrayList<>());
+
+        while (!queue.isEmpty()) {
+            TreeNode top = queue.poll();
+
+            if (top == null) {
+                if (!queue.isEmpty()){
+                    queue.add(null);
+                    res.add(new ArrayList<>());
+                }
+                continue;
+            } else {
+                res.get(res.size() - 1).add(top.val);
+            }
+
+            if (top.left != null)
+                queue.add(top.left);
+            if (top.right != null)
+                queue.add(top.right);
+        }
+
+        return res;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode top = queue.poll();
+                list.add(top.val);
+                if (top.left != null)
+                    queue.add(top.left);
+                if (top.right != null)
+                    queue.add(top.right);
+            }
+
+            res.add(list);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(7);
+
+        treeNode.left.left = new TreeNode(4);
+        treeNode.left.right = new TreeNode(5);
+
+
+        treeNode.right.left = new TreeNode(7);
+        treeNode.right.right = new TreeNode(1);
+        new BinaryTreeLevelOrderTraversal102().levelOrder(treeNode);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/main/java/leetcode/queue/PerfectSquares279.java
@@ -0,0 +1,62 @@
+package leetcode.queue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @program: interview
+ * @author: fanmk
+ * @email: comeandgo2014@gmail.com
+ * @decription: https://leetcode.com/problems/perfect-squares (利用队列的bfs解法)
+ * @date: 2020/4/4 22:47
+ * @see leetcode.dp.PerfectSquares279 动态规划解法
+ */
+public class PerfectSquares279 {
+    static class Solution {
+        //dp解法 fn = f(n-i*i) +1;
+        public int numSquares(int n) {
+            LinkedList<Node> queue = new LinkedList<>();
+            queue.add(new Node(n, 0));
+
+            //记录节点是否访问过，因为层次遍历，节点加入队列的时候就是step最短的时候，后续无需再加入
+            boolean[] visited = new boolean[n + 1];
+            visited[n] = true;
+
+            while (!queue.isEmpty()) {
+                Node cur = queue.remove();
+
+                int i = 1;
+                int tmp;
+                while ((tmp = cur.val - i * i) >= 0) {
+                    if (tmp == 0) {
+                        return cur.step + 1;
+                    }
+
+                    if (!visited[tmp]) {
+                        queue.add(new Node(tmp, cur.step + 1));
+                        visited[tmp] = true;
+                    }
+                    i++;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    static class Node {
+        int val;
+        int step;
+
+        Node(int val, int step) {
+            this.step = step;
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().numSquares(12));
+    }
+}
