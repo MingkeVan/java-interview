e59696070f7cdd4da1f8c65f003d3657d8760e4b
fanmk <877148452@qq.com>
2021-01-04 17:11:00 +0800

    1.优化单词计数

--- src/main/java/com/fanmk/interview/ali/FileHelper.java
+++ src/main/java/com/fanmk/interview/ali/FileHelper.java
@@ -1,6 +1,5 @@
 package com.fanmk.interview.ali;
 
-
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -10,118 +9,25 @@ import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 
-/**
- * 统计一篇文章中每个单词出现的次数（文章大小超过10G），
- * 例如输入：
- * If not now when , if not me who?
- * 输出
- * if 2
- * not 2
- * now 1
- * when 1
- * me 1
- * who 1
- * 备注：文章已做过预处理，已将所有标点符号替换为空格。用多线程处理。
- */
 public class FileHelper {
-    static class FileSplit {
-        long start;
-        long size;
 
-        FileSplit(long start, long size) {
-            this.start = start;
-            this.size = size;
-        }
-    }
-
-    public static void generateBigFile() throws IOException {
-        File file = new File("./big_file_10G.txt");
+    /**
+     * 将传入字符串重复指定次数，模拟大文件
+     * @param str
+     * @param count
+     * @param filePath
+     * @throws IOException
+     */
+    public static void generateBigFile(String str, long count,String filePath) throws IOException {
+        File file = new File(filePath);
         FileWriter fileWriter = new FileWriter(file);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        String str = "apple bad big append map many much pod pod bad this that where when append";
-        for (int i = 0; i < 1000000000; i++) {
+        for (int i = 0; i < count; i++) {
             bufferedWriter.write(str);
             bufferedWriter.newLine();
         }
         bufferedWriter.flush();
         bufferedWriter.close();
-    }
-
-    /**
-     * 将指定文件拆分为指定数目的小文件
-     *
-     * @param filePath
-     * @return 分割的文件路径列表
-     */
-    public static List<FileSplit> splitFile(String filePath,
-                                            int fileCount,
-                                            CountDownLatch countDownLatch,
-                                            Executor executor) throws IOException {
-        FileInputStream fis = new FileInputStream(filePath);
-        FileChannel inputChannel = fis.getChannel();
-        final long fileSize = inputChannel.size();
-        long average = fileSize / fileCount;//平均值
-        long bufferSize = 200; //缓存块大小，自行调整
-        ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.valueOf(bufferSize + "")); // 申请一个缓存区
-        long startPosition = 0; //子文件开始位置
-        long endPosition = average < bufferSize ? 0 : average - bufferSize;//子文件结束位置
-        for (int i = 0; i < fileCount; i++) {
-            if (i + 1 != fileCount) {
-                int read = inputChannel.read(byteBuffer, endPosition);// 读取数据
-                readW:
-                while (read != -1) {
-                    byteBuffer.flip();//切换读模式
-                    byte[] array = byteBuffer.array();
-                    for (int j = array.length - 1; j >= 0; j--) {
-                        byte b = array[j];
-                        if (b == 10 || b == 13 || b == 32) { //判断\n\r
-                            endPosition += j;
-                            break readW;
-                        }
-                    }
-                    endPosition += bufferSize;
-                    byteBuffer.clear(); //重置缓存块指针
-                    read = inputChannel.read(byteBuffer, endPosition);
-                }
-            } else {
-                endPosition = fileSize; //最后一个文件直接指向文件末尾
-            }
-
-            byteBuffer.get();
-            byte[] newarr = new byte[Integer.valueOf(endPosition - startPosition + "")];
-            System.arraycopy();
-            executor.execute(
-                    new CountWordThread(
-                            countDownLatch,
-                            i,
-                            new String(),
-                            fileList.get(i).start,
-                            fileList.get(i).size,
-                            res)
-            );
-
-            startPosition = endPosition + 1;
-            endPosition += average;
-        }
-
-        inputChannel.close();
-        fis.close();
-        return splitFileList;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-
-        generateBigFile();
-
-//        Scanner scanner = new Scanner(System.in);
-//        scanner.nextLine();
-//        long startTime = System.currentTimeMillis();
-//        List<String> list = splitFile("./small_file.txt");
-//        long endTime = System.currentTimeMillis();
-//        System.out.println("耗费时间： " + (endTime - startTime) + " ms");
-//        System.out.println(list.size());
-//        scanner.nextLine();
     }
 
 }
--- src/main/java/com/fanmk/interview/ali/WordCount.java
+++ src/main/java/com/fanmk/interview/ali/WordCount.java
@@ -1,7 +1,9 @@
 package com.fanmk.interview.ali;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,43 +12,169 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.LongAdder;
 
+import static com.fanmk.interview.ali.FileHelper.generateBigFile;
+
+/**
+ * 统计一篇文章中每个单词出现的次数（文章大小超过10G），
+ * 例如输入：
+ * If not now when , if not me who?
+ * 输出
+ * if 2
+ * not 2
+ * now 1
+ * when 1
+ * me 1
+ * who 1
+ * 备注：文章已做过预处理，已将所有标点符号替换为空格。用多线程处理。
+ * <p>
+ * 思路：
+ * 1. 主线程通过FileChannel读取文件，将文件拆分为指定数目的子任务
+ * 2. 主线程构建全局map, 利用concurrentHashMap和LongAdder进行多线程下的单词计数
+ * 3. 由5个线程的线程池执行单词计数的子任务
+ * 3.1 子线程将传入的字符串按空格拆分，并计数
+ * 3.2 子线程完成计数后，countDownLatch.countDown()
+ */
 public class WordCount {
 
-    public static Map<String, LongAdder> countWords(String filePath) throws InterruptedException, IOException {
-        File file = new File(filePath);
-        //1. 分割文件
-        System.out.println("开始分割文件");
-        long size = 20 * 1024 * 1034; // 拆分为20mb
-        List<FileHelper.FileSplit> fileList = FileHelper.splitFile(filePath,size);
-        System.out.println("完成分割文件，总数为" + fileList.size());
+    static CountDownLatch countDownLatch;
+    static Executor executor;
+    static Map<String, LongAdder> res;
 
-        //2. 构造线程数为5的线程池
+    public static Map<String, LongAdder> countWords(String filePath, Integer splitCount) throws InterruptedException, IOException {
+
+        //1. 构造线程数为5的线程池
         int threadCount = 5;
-        Executor executor = Executors.newFixedThreadPool(threadCount);
+        executor = Executors.newFixedThreadPool(threadCount);
 
-        //3. 声明返回值map
-        Map<String, LongAdder> res = new ConcurrentHashMap<>();
+        //2. 声明返回值map
+        res = new ConcurrentHashMap<>();
 
-        // 通过CountDownLatch计数
-        CountDownLatch countDownLatch = new CountDownLatch(fileList.size());
-        for (int i = 0; i < fileList.size(); i++) {
-            executor.execute(
-                    new CountWordThread(
-                            countDownLatch,
-                            i,
-                            file,
-                            fileList.get(i).start,
-                            fileList.get(i).size,
-                            res)
-            );
-        }
+        // 3.将文件计数拆分为若干个任务，通过CountDownLatch计数
+        int taskCount = splitCount == null ? 10000 : splitCount;
+        countDownLatch = new CountDownLatch(taskCount);
+
+        //4. 分割文件
+        splitFile(filePath, taskCount);
         countDownLatch.await();
 
         return res;
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Map<String, LongAdder> res = countWords("./big_file_10G.txt");
-        System.out.println(res);
+    /**
+     * 将指定文件拆分为指定数目的小文件
+     *
+     * @param filePath
+     * @return 分割的文件路径列表
+     */
+    public static void splitFile(String filePath, int fileCount) throws IOException {
+        FileInputStream fis = new FileInputStream(filePath);
+        FileChannel inputChannel = fis.getChannel();
+        final long fileSize = inputChannel.size();
+        long average = fileSize / fileCount;//平均值
+        long bufferSize = 200; //缓存块大小，自行调整
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.valueOf(bufferSize + "")); // 申请一个缓存区
+
+        ByteArrayOutputStream tmpOutputStream = new ByteArrayOutputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        for (int i = 0; i < fileCount; i++) {
+            outputStream.reset();
+
+            // 将上一次剩余的字节放入本次字节流
+            if (tmpOutputStream.size() != 0) {
+                tmpOutputStream.writeTo(outputStream);
+                tmpOutputStream.reset();
+            }
+
+            if (i + 1 != fileCount) {
+                int read = inputChannel.read(byteBuffer);// 读取数据
+                readW:
+                while (read != -1) {
+                    byteBuffer.flip();//切换读模式
+                    byte[] array = byteBuffer.array();
+
+                    if (outputStream.size() + bufferSize > average) {
+                        for (int j = read - 1; j >= 0; j--) {
+                            byte b = array[j];
+
+                            // 判断空格 换行等 避免截断
+                            if (b == 10 || b == 13 || b == 32) {
+
+                                //将前半部分数组，写入输出字节流
+                                outputStream.write(array, 0, j);
+                                // 将后半部分数组，写入缓存字节流
+                                tmpOutputStream.write(array, j, read - j);
+
+                                break readW;
+                            }
+                        }
+                    }
+
+                    // 将整个字节数组写入字节流
+                    outputStream.write(array);
+
+                    byteBuffer.clear(); //重置缓存块指针
+                    read = inputChannel.read(byteBuffer);
+                }
+            } else {
+                int read = inputChannel.read(byteBuffer);// 读取数据
+
+                while (read != -1) {
+                    byteBuffer.flip();//切换读模式
+                    byte[] array = byteBuffer.array();
+
+                    // 将整个字节数组写入字节流
+                    outputStream.write(array);
+
+                    byteBuffer.clear(); //重置缓存块指针
+                    read = inputChannel.read(byteBuffer);
+                }
+
+            }
+
+            if (outputStream.size() != 0) {
+                executor.execute(new CountWordThread(
+                        countDownLatch,
+                        i,
+                        outputStream.toString(),
+                        res)
+                );
+
+            }
+        }
+
+        inputChannel.close();
+        fis.close();
     }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // 构造大文件
+        int lineCnt = 1000 * 1000 * 1000;
+//
+//        long start = System.currentTimeMillis();
+//        generateBigFile("If not now when if not me who", lineCnt, "./big_file_10G.txt");
+//        long end = System.currentTimeMillis();
+//        System.out.println("生成大文件耗时：" + (end - start));
+
+        // 用例1: 小文件
+        long start = System.currentTimeMillis();
+        Map<String, LongAdder> res = countWords("./small_file.txt", 1);
+        long end = System.currentTimeMillis();
+        System.out.println("单词计数耗时：" + (end - start));
+
+        assert res.get("now").longValue() == 1;
+
+        long start1 = System.currentTimeMillis();
+        System.out.println("开始多线程单词计数");
+
+        Map<String, LongAdder> res1 = countWords("./big_file_10G.txt", 10000);
+
+        long end1 = System.currentTimeMillis();
+        System.out.println("结束多线程单词计数");
+        System.out.println("多线程单词计数耗时：" + (end1 - start1));
+
+        assert res1.get("now").longValue() == lineCnt;
+    }
+
 }
